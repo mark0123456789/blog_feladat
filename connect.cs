860b9debaad7650aad41cf4b8a3158d18829b8a9
mark <piringerm@kkszki.hu>
2025-02-14 12:56:06 +0100

    A feladat alapjai készek

---
+++ connect.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
+using DocumentFormat.OpenXml.Drawing.Diagrams;
+
+namespace blog_feladat
+{
+    public class connect
+    {
+        public MySqlConnection connection;
+        public string Host;
+        public string Database;
+        public string User;
+        public string Password;
+        public string ConnectionString;
+
+
+
+        public connect()
+        {
+            Host = "localhost";
+            Database = "blog";
+            User = "root";
+            Password = "";
+
+            ConnectionString = "SERVER=" + Host + ";DATABASE=" + Database + ";UID=" + User + ";PASSWORD=" + Password + ";SslMode=None";
+
+
+
+            connection = new MySqlConnection(ConnectionString);
+        }
+    }
+}
